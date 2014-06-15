62d53f62762c696731aace0017247dca5456d90e
Agustín <hodesagustin@gmail.com>
2014-06-14 21:02:38 -0300

    Agrego DROP de GuardarEmpresa

--- data/fleta_tablas.sql
+++ data/fleta_tablas.sql
@@ -44,6 +44,7 @@ DROP PROCEDURE LOL.sp_CrearPublicacion
 DROP PROCEDURE LOL.sp_InsertPublicacionRubro 
 DROP PROCEDURE LOL.sp_InsertarCliente
 DROP PROCEDURE LOL.sp_GuardarCliente
+DROP PROCEDURE LOL.sp_GuardarEmpresa
 
 --Eliminar esquema
 DROP SCHEMA LOL
